8ad38e698a858b9e4fd112467f46e9123b12d6fd
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-04-22 18:48:40 +0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,15 +1,16 @@
 //Select elements
 const input_element = document.querySelector('.input');
-const output_operation_element = document.querySelector('.operation value');
-const output_result_element = document.querySelector('.result value');
+const output_operation_element = document.querySelector('.operation .value');
+const output_result_element = document.querySelector('.result .value');
 
 //Some variables
 const OPERATORS = ["+", "-", "*", "/"];
-const POWER = "POWER(", FACTORIAL = "FACTORIAL";
+const POWER = "POWER(", FACTORIAL = "FACTORIAL(";
 let data = {
     operation: [],
     formula: []
 }
+let ans = 0;
 
 // Calculator buttons
 let calculator_buttons = [
@@ -225,23 +226,73 @@ let calculator_buttons = [
 ];
 
 //Button appear
-function createCalculatorButtons() {
-    const bnts_per_row = 8;
-    let added_btns = 0;
+
+function createCalculatorbutton() {
+    const buttons_per_row = 8
+    let added_buttons = 0
 
     calculator_buttons.forEach(button => {
-        if (added_btns % bnts_per_row == 0) {
-            input_element.innerHTML += `<div class="row"></div>`;
+        if (added_buttons % buttons_per_row == 0) {
+            input_element.innerHTML += `<div class = "row"></div>`
         }
 
-        const row = document.querySelector(".row:last-child");
-        row.innerHTML += `<button id="$(button.name)">
-            ${button.symbol}
-        </button>`;
-        added_btns++;
+        const row = document.querySelector(".row:last-child")
+        row.innerHTML += `<button id = ${button.name}>
+        ${button.symbol}
+        </button>`
+
+        added_buttons++
     })
 }
-createCalculatorButtons();
+
+createCalculatorbutton()
+
+// CLICK EVENT LISTENER
+input_element.addEventListener("click", event => {
+    const target_btn = event.target;
+    calculator_buttons.forEach(button => {
+        if (button.name == target_btn.id) calculator(button);
+    })
+})
+
+//CALCULATOR 
+function calculator(button) {
+    if (button.type == "operator") {
+        data.operation.push(button.symbol);
+        data.formula.push(button.formula);
+
+    } else if (button.type == "number") {
+        data.operation.push(button.symbol);
+        data.formula.push(button.formula);
+
+    } else if (button.type == "trigo_function") {
+
+    } else if (button.type == "math_function") {
+
+    } else if (button.type == "key") {
+
+    } else if (button.type == "calculate") {
+        formula_str = data.formula.join('');
+        let result = eval(formula_str);
+        updateOutputResult(result);
+    }
+
+    updateOutputOperation(data.operation.join(''));
+}
+
+//Update Output
+function updateOutputOperation(operation) {
+    output_operation_element.innerHTML = operation
+}
+function updateOutputResult(result) {
+    output_result_element.innerHTML = result
+}
+
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/* !!!!!!!!!!!!!!!THIS IS WHERE I LEFT !!!!!!!!!!!!!*/
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
 // GAMMA FUNCTINON
 function gamma(n) {  // accurate to about 15 decimal places
